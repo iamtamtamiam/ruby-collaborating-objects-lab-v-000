191208ef192e5888b091716af84c4a911aa22ba7
Tam Vu <vutam_a@hotmail.com>
2019-08-13 01:12:16 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -12,7 +12,7 @@ class MP3Importer
  end 
  
  def import
-   self.files.each do |file|  #why self?
+   self.files.each do |file|  #why self? why not @files? 
      Song.new_by_filename(file)
     end 
  end 
